9251d65758d504a61e11daa029d6cb2d558be508
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2024-09-08 20:39:40 -0500

    feat: created the first view from the webpage

---
+++ app/routes/main.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, render_template
+from flask_bcrypt import Bcrypt
+
+# Init encrypt the data
+bcrypt = Bcrypt()
+
+# Created a blueprint 
+main = Blueprint('main', __name__)
+
+# HomePage
+@main.route('/')
+def home():
+    return render_template('main.html')
