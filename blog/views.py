bc6f8bf78069ffd2729cfb937391e4d99827fe1f
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2024-09-30 21:25:49 -0500

    feat: Start working with templates and views

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from blog.models import blog_entry
 
 # GET Method
 def blog(request):
-    return render(request=request, template_name='blog.html')
+    blogs_title_entry = blog_entry.objects.values('title', 'created_at')
+    return render(request=request, template_name='blog.html', context={
+        "blogs_entry_titles": blogs_title_entry
+    })
