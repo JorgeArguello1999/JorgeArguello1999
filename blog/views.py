9b12d180e8c9169aed3e92ae6e6f2d0fba63d79b
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2024-10-03 16:36:53 -0500

    feat: Updated the initial blog page with id

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from blog.models import blog_entry
 
 # GET Method
 def blog(request):
-    blogs_title_entry = blog_entry.objects.values('title', 'created_at')
+    blogs_title_entry = blog_entry.objects.values('title', 'created_at', 'blog_id', 'image_url')
     return render(request=request, template_name='blog.html', context={
         "blogs_entry_titles": blogs_title_entry
     })
