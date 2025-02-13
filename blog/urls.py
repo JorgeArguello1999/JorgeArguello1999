7f281678c40b97dad432a676e7badcd5d78f8f96
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2025-02-13 16:59:26 -0500

    RESET

--- blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from blog import views
-
-urlpatterns = [
-    path('', views.blog, name='blog'),
-    path('<int:blog_id>/', views.blog_detail, name="blog_detail")
-]
--- experiences/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from experiences.models import Experiences
-
-# Register your models here.
-admin.site.register(Experiences)
